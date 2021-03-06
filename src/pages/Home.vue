643be903ed5086fa928dade49fa78e3d24f2f641
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2021-03-06 12:57:26 +0200

    feat(pages.home): add divider after form

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,6 +3,12 @@
     <div class="q-py-lg q-px-md">
       <new-tweet-form />
     </div>
+
+    <q-separator
+      class="divider"
+      size="10px"
+      color="grey-2"
+    />
   </q-page>
 </template>
 
@@ -15,3 +21,11 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.divider {
+  border-top: 1px solid;
+  border-bottom: 1px solid;
+  border-color: $grey-4;
+}
+</style>
